54458e9e6c542d00f0b2c0b78e6956a09dc9edac
pkdahl <pkdahl@gmail.com>
2015-11-03 16:54:46 +0100

    URLs for forms, moved code in views and tests

--- superlists/urls.py
+++ superlists/urls.py
@@ -8,11 +8,12 @@ urlpatterns = [
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$',
-        views.home_page,
-        name='home'),
-    url(r'^lists/the-only-list-in-the-world/$',
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
+    url(
+        r'^lists/the-only-list-in-the-world/$',
         views.view_list,
-        name='view_list'),
+        name='view_list'
+    ),
     # url(r'^admin/', include(admin.site.urls)),
 ]
